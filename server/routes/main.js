1207b209b8ce6a7296a7d3cbd5a64b043f58c2ee
Ishan-Vimukthi <ishanvimukthie@gmail.com>
2024-09-20 13:37:14 +0530

    read

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const post = require('../models/post');
 
 // Routers
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDB."
+    }
+
+    try{
+        const data = await post.find();
+        res.render('index', {locals, data});
+    } catch (error) {
+        console.log(error);
+    }
+    
+
 });
+
+function insertPostData(){
+    post.insertMany([
+        {
+            title: "Building a Blog",
+            body: "This is the body text"
+        },
+    ])
+}
+
+insertPostData();
 
 router.get('/about', (req, res) => {
     res.render('about');
